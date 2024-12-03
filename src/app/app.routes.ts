612567318e1b76d1ded7b81634a1992afca3fb08
Joao Antonio <joaoantoniosla@gmail.com>
2024-12-03 16:19:24 -0300

    app.component, app config e app routes atualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {provideRouter, Routes} from '@angular/router';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: PostListComponent},
+    { path: 'add', component: PostFormComponent },
+    { path: 'edit/:id', component: PostFormComponent }    
+];
+
+export const appRouterProviders = [provideRouter(routes)];
